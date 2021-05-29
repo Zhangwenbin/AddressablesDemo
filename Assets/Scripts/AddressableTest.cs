eb4f8831a9dbdd6a5cbbbf51c6b3bcf5de3568a5
zhangwenbin <991355745@qq.com>
2021-05-29 20:59:04 +0800

    第一版完成

---
+++ Assets/Scripts/AddressableTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.Video;
+using UnityEngine.UI;
+public class AddressableTest : MonoBehaviour
+{
+    public Text statusText, updateCountText, updateSizeText, lsText,ldText, rsText, rdText;
+
+    public string lsKey,ldKey, rsKey, rdKey;
+
+    private void Start()
+    {
+        statusText.text = "InitializeAsync";
+        Addressables.InitializeAsync().Completed += initialCompleted;
+    }
+    
+    private void initialCompleted(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        statusText.text = "CheckForCatalogUpdates";
+        Addressables.CheckForCatalogUpdates(true).Completed += checkComplete;
+    }
+
+    private void checkComplete(AsyncOperationHandle<List<string>> obj)
+    {
+        statusText.text = "checkComplete";
+        if (obj.Status == AsyncOperationStatus.Failed)
+        {
+            return;
+        }
+        updateCountText.text = obj.Result.Count+"";
+        Debug.Log(obj.Result.Count);
+        if (obj.Result.Count == 0)
+        {
+            return;
+        }
+
+        Addressables.UpdateCatalogs(obj.Result).Completed += updatecomplete;
+    }
+
+
+
+    private void updatecomplete(AsyncOperationHandle<List<IResourceLocator>> obj)
+    {
+        Debug.Log(obj.Status);
+        if (obj.Status == AsyncOperationStatus.Failed)
+        {
+            return;
+        }
+
+        long size = 0;
+        var locators = Addressables.ResourceLocators;
+        foreach (var item in locators)
+        {
+            var keys = item.Keys;
+            foreach (var key in keys)
+            {
+                Addressables.GetDownloadSizeAsync(key).Completed += (res) => {
+                    size += res.Result;
+                };
+            }
+        }
+        updateSizeText.text = size + "";
+        Debug.Log(size);
+    }
+
+    public void TestASync()
+    {
+        AssetManager.Instance.LoadAssetAsync<TextAsset>(lsKey, (res, op) =>
+        {
+            Debug.Log(res.text);
+            lsText.text = res.text;
+        });
+        AssetManager.Instance.LoadAssetAsync<TextAsset>(ldKey, (res, op) =>
+        {
+            Debug.Log(res.text);
+            ldText.text = res.text;
+        });
+        AssetManager.Instance.LoadAssetAsync<TextAsset>(rsKey, (res, op) =>
+        {
+            Debug.Log(res.text);
+            rsText.text = res.text;
+        });
+        AssetManager.Instance.LoadAssetAsync<TextAsset>(rdKey, (res, op) =>
+        {
+            Debug.Log(res.text);
+            rdText.text = res.text;
+        });
+    }
+
+    public void DownLoadAll()
+    {
+        var locators = Addressables.ResourceLocators;
+        foreach (var item in locators)
+        {
+            var keys = item.Keys;
+            foreach (var key in keys)
+            {
+                Addressables.DownloadDependenciesAsync(key).Completed += (res) => {
+                   
+                };
+            }
+        }
+    }
+
+
+
+}
